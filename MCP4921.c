7adbbeb021043bcdc7eeccc126de47a0bf4f34c1
AudioDIWHY <40243825+cslammy@users.noreply.github.com>
2022-03-04 06:31:45 -0800

    bug fixes

--- MCP4921.c
+++ MCP4921.c
@@ -11,7 +11,7 @@ spi_mode(0);
 
 
 
- *  Author: audioDIWHY
+ *  Author: clamm
 */
 
 #include <stdio.h>
@@ -20,49 +20,29 @@ spi_mode(0);
 
 uint8_t MSBwcontrol = 0;
 
-void write4921(uint16_t data)
+void write4921(uint8_t MSB, uint8_t LSB)
     {
 	
-    {
-	
-        // code assumes LDAC (4921 pin 5) tied to ground
-		uint16_t data12 = data & 0b0000111111111111; // we only want 12 LSBs
+
 		
 		
 
-	
-		uint8_t MSB = (data12 >> 8);
-		MSB |= (1 << 6) | (1 << 5) | (1 << 4) ; // buffered ref in, gain = ref; no shutdown
- 
-		MSB &= ~(1 << 7);  // this control must be zero always
- 
-		uint8_t LSB = data12 & 0x00FF;
-		
-		
-		
-		
-
- 
+		MSBwcontrol = 0b01110000 + MSB; // control bits: write to DAC; buffer out; normal gain; no shutdown;  bit 15 is always 0
 		
 		
 	    // for 4921, you send 2 bytes.  bit 15 is always zero,
-	    // bits 14,13,12 are control, 11-10-9-8 are MSBs data 
-		//  7 6 5 4 3 2 1 0 are LSB data
-	    
+	    // bits 14,13,12 are control, 11,10,9,8 forms MSB of data sent.
+	    // bits 7-0 are LSB.
 	     
 
 	    //send the data.  4921 can eat 16 bit xfers.
 	    SELECT();
 
-        SPI_SendByte(MSB);
-      //  SPI_SendByte(0b01110111);
-		SPI_SendByte(LSB);
-	 //   SPI_SendByte(0x00);
-		DESELECT();
-	
-	    
-    }
+        SPI_SendByte(MSBwcontrol);
 
+		SPI_SendByte(LSB);
+	    
+		DESELECT();
 	
 	    
     }
